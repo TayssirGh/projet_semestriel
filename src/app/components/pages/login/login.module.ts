3212ff9b63ed4e2577f194037a9e9694a5d2e8b5
TayssirGh <tayssir.ghanmi@eniso.u-sousse.tn>
2023-03-30 01:41:00 +0100

    routing

---
+++ src/app/components/pages/login/login.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { LoginRoutingModule } from "./login-routing.module";
+import { LoginComponent } from "./login.component";
+import { CommonModule } from "@angular/common";
+@NgModule({
+    imports: [
+        CommonModule,
+        LoginRoutingModule,
+    ],
+    declarations: [ LoginComponent],
+    
+})
+export class LoginModule { }
