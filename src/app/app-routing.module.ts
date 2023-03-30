23232b8b9a222f1f9b2aade29515f318e73232d3
TayssirGh <tayssir.ghanmi@eniso.u-sousse.tn>
2023-03-30 01:36:42 +0100

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
-const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    { path : 'login', loadChildren: () => import('./components/pages/login/login.module').then(m => m.LoginModule) },
+  ])
+],
   exports: [RouterModule]
-})
-export class AppRoutingModule { }
+},)
+export class AppRoutingModule {}
