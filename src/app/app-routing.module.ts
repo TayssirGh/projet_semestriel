e807af4ed0fdc7d7c89e69f582018b47462c87e4
GaidAdam <117405327+GaidAdam@users.noreply.github.com>
2023-04-13 01:17:34 +0100

    heyy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { RouterModule } from '@angular/router';
   imports: [RouterModule.forRoot([
     { path : 'login', loadChildren: () => import('./components/pages/login/login.module').then(m => m.LoginModule) },
     { path : 'formStartup', loadChildren: () => import('./components/pages/form-startup/formStartup.module').then(m => m.formStartupModule) },
-    { path : 'raisePage', loadChildren: () => import('./components/pages/raise-page/raise-page.module').then(m => m.RaisePageModule) }
+    { path : 'raisePage', loadChildren: () => import('./components/pages/raise-page/raise-page.module').then(m => m.RaisePageModule) },
+    { path : 'invest', loadChildren: () => import('./components/pages/invest/invest.module').then(m => m.investModule) }
   ])
 ],
   exports: [RouterModule]
