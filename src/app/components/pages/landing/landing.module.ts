d7445ea62488d064eb50b6b9a6e6dfeb920dc99f
TayssirGh <tayssir.ghanmi@eniso.u-sousse.tn>
2023-04-13 01:18:50 +0100

    landing 🐧

---
+++ src/app/components/pages/landing/landing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+
+import { LandingComponent } from "./landing.component";
+import { LandingRoutingModule } from "./landing-routing.module";
+
+
+
+@NgModule({
+    imports: [
+        LandingRoutingModule,
+       
+    ],
+    declarations: [ LandingComponent],
+    
+})
+export class LandingModule { }
