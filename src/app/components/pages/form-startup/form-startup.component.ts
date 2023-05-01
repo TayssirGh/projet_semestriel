252ca9d3523bc300e7bb04306112cc8bc885432e
GaidAdam <117405327+GaidAdam@users.noreply.github.com>
2023-05-01 14:26:16 +0100

    post/get

--- src/app/components/pages/form-startup/form-startup.component.ts
+++ src/app/components/pages/form-startup/form-startup.component.ts
@@ -1,15 +1,35 @@
 import { Component } from '@angular/core';
+import { CreateService } from 'src/app/services/create/create.service';
 
 @Component({
   selector: 'app-form-startup',
   templateUrl: './form-startup.component.html',
-  styleUrls: ['./form-startup.component.scss']
+  styleUrls: ['./form-startup.component.scss'],
+  providers : [CreateService]
 })
 export class FormStartupComponent {
- log(x){
-  console.log(x)
+  constructor(private service: CreateService){}
+  createStartup(startup:{name: String ,description :String ,industry:String, imageUrl:String }){
+    this.service.createStartup(startup).subscribe(( response )=> {
+    console.log(response);
+     }),
+     ( error )=> { console.log(error);}
 
-  };
+
+
+     }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
